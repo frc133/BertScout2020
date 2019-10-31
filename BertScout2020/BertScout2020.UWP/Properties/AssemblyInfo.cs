70fcb5e0e229145fd102d9fa20ecfe5d4a6a3b5a
Scott Bakker <scott@bakkergames.com>
2019-10-30 23:00:41 -0400

    Updated version

--- BertScout2020/BertScout2020.UWP/Properties/AssemblyInfo.cs
+++ BertScout2020/BertScout2020.UWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BertScout2020.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Bonny Eagle Robotics Team 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2019.10.30.2251")]
+[assembly: AssemblyFileVersion("2019.10.30.2251")]
 [assembly: ComVisible(false)]
