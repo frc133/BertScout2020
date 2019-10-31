45cdd8744e69e51563cf64ba23a55b56041ebc2e
Scott Bakker <scott@bakkergames.com>
2019-10-30 22:04:30 -0400

    have three projects compiling

--- BertScout2020Data/Properties/AssemblyInfo.cs
+++ BertScout2020Data/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BertScout2020Data")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("180df173-92f6-45c4-9e4b-ae81568f507b")]
+[assembly: Guid("401ab956-752c-48ee-a403-42752da820e3")]
 
 // Version information for an assembly consists of the following four values:
 //
